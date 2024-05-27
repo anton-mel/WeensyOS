04d317e2cd7c33f093292da2d96630d74e3480a5
Anton Melnychuk <anton.melnychuk3@gmail.com>
2024-05-27 04:21:30 -0400

    Fix Testing

--- starter-code-rust/tests/heap_allocation.rs
+++ starter-code-rust/tests/heap_allocation.rs
@@ -1,24 +1,24 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(blog_os::test_runner)]
+#![test_runner(weensyos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 extern crate alloc;
 
 use alloc::{boxed::Box, vec::Vec};
-use blog_os::{allocator::HEAP_SIZE, serial_print, serial_println};
+use weensyos::{allocator::HEAP_SIZE, serial_print, serial_println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 entry_point!(main);
 
 fn main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::allocator;
-    use blog_os::memory::{self, BootInfoFrameAllocator};
+    use weensyos::allocator;
+    use weensyos::memory::{self, BootInfoFrameAllocator};
     use x86_64::VirtAddr;
 
-    blog_os::init();
+    weensyos::init();
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
@@ -72,5 +72,5 @@ fn many_boxes_long_lived() {
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
+    weensyos::test_panic_handler(info)
 }
