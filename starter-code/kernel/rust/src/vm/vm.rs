3484c0e6de058a11524caf1727ced5b727ab0a12
anton-mel <anton.melnychuk3@gmail.com>
2025-03-17 17:27:35 -0400

    Add Rust lookup_l1pagetable to the Starter Code

--- starter-code/kernel/rust/src/vm/vm.rs
+++ starter-code/kernel/rust/src/vm/vm.rs
@@ -1,5 +1,60 @@
 use crate::vm::*;
 
+// lookup_l1pagetable(pagetable, va, perm)
+//    Helper function to find the last level of `va` in `pagetable`
+//
+//    Returns an x86_64_pagetable pointer to the last level pagetable
+//    if it exists and can be accessed with the given permissions
+//    Returns NULL otherwise
+
+#[no_mangle]
+#[allow(unused_variables)] // must be removed
+pub unsafe extern "C" fn lookup_l1pagetable(
+    pagetable: *mut x86_64_pagetable,
+    va: usize,
+    perm: i32,
+) -> *mut x86_64_pagetable {
+    let mut pt = pagetable;
+
+    // We find the L1 pagetable by doing the following three steps for each level
+    // 1. Find index to the next pagetable entry using the `va`
+    // 2. Check if this entry has the appropriate requested permissions
+    // 3. Repeat the steps till you reach the L1 pagetable (i.e., thrice)
+    // 4. Return the pagetable address
+
+    for i in 0..=2 {
+        // TODO
+        // find page entry by finding `ith` level index of va to index pagetable entries of `pt`
+        // you should read x86-64.h to understand relevant structs and macros to make this part easier
+        let pe: u64 = 0; // replace this
+
+        if (pe & PTE_P as u64) == 0
+        { // address of next level should be present AND PTE_P should be set, error otherwise
+            // HERE NEED TO ADD THE LOGPRINTF
+            if (perm & PTE_P as i32) == 0 {
+                return core::ptr::null_mut();
+            }
+            // HERE NEED TO ADD THE LOGPRINTF
+            return core::ptr::null_mut();
+        }
+
+        // sanity-check page entry and permissions
+        assert!(pte_addr(pe) < MEMSIZE_PHYSICAL as u64); // at sensible address
+        if (perm & PTE_W as i32) != 0 {
+            assert!((pe & PTE_W as u64) != 0); // if requester wants PTE_W, entry must allow PTE_W
+        }
+        if (perm & PTE_U as i32) != 0 {
+            assert!((pe & PTE_U as u64) != 0); // if requester wants PTE_U, entry must allow PTE_U
+        }
+
+        // TODO
+        // set pt to physical address to next pagetable using `pe`
+        pt = core::ptr::null_mut(); // replace this
+    }
+
+    pt
+}
+
 // virtual_memory_map(pagetable, va, pa, sz, perm)
 //    Map virtual address range `[va, va+sz)` in `pagetable`.
 //    When `X >= 0 && X < sz`, the new pagetable will map virtual address
